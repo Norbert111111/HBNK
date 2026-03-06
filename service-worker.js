a072c19014ed684b2a9562af2b923a821d81ab2d
Norbert <norberthabamungu22@gmail.com>
2026-03-06 16:31:26 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,28 @@
 const CACHE_NAME = "hbnk-pwa-v5";
+
 const urlsToCache = [
-  "/HBNK/",
-  "/HBNK/index.html",
-  "/HBNK/HBNK.html",
-  "/HBNK/index3.html",
-  "/HBNK/style.css",
-  "/HBNK/style2.css",
-  "/HBNK/style3.css",
-  "/HBNK/icon-192.png",
-  "/HBNK/icon-512.png",
-  "/HBNK/manifest.json"
+  "./",
+  "./index.html",
+  "./HBNK.html",
+  "./index3.html",
+  "./style3.css",
+  "./style2.css",
+  "./style4.css",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
